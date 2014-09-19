80e17e7da0a75483a5e2d5a1f0855f20b129a4f2
Berwin <liubowen@liubowendeMacBook-Air.local>
2014-09-19 23:39:42 +0800

    md5

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var config = require( './config' );
 var path = require( 'path' );
 var bodyParser = require('body-parser');
 var favicon = require('static-favicon');
+var cookieParser = require('cookie-parser');
 
 app.set( 'views', path.join( __dirname, 'views' ) );
 app.set( 'view engine', 'ejs' );
@@ -15,6 +16,7 @@ app.use( favicon() );
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded() );
+app.use( cookieParser() );
 app.use( express.static( path.join( __dirname, 'public' ), { maxAge: 86400 } ) );
 app.use( '/', router );
 
--- config.default.js
+++ config.default.js
@@ -1,5 +1,6 @@
 'use strict';
 module.exports = {
     LISTEN : 9000,
-    MONGO_RUL : 'mongodb://127.0.0.1:27017/demos'
+    MONGO_RUL : 'mongodb://127.0.0.1:27017/demos',
+    MD5_SUFFIX : 'Encryption'
 };
