931598fd1d34323a289cd9fa46cf4cc8d91da093
berwin <berwin1995@qq.com>
2015-01-05 18:01:48 +0800

    update tool cookieToObject function

--- public/js/tool.js
+++ public/js/tool.js
@@ -23,7 +23,7 @@ var util = {
     },
     cookieToObject : function (str) {
         var obj = {};
-        var arr = str.split( ';' );
+        var arr = str.split( '; ' );
         for( var i = 0; i < arr.length; i++ ){
             var aCookie = arr[i].split( '=' );
             obj[ aCookie[0] ] = aCookie[1];
