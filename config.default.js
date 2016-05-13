5f7ce57566eefa2d959935c8776813a69e67f052
berwin <berwin1995@qq.com>
2016-05-13 12:32:02 +0800

    清除1.0版本的所有代码

--- config.default.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {
-    LISTEN : 9000,
-    MONGO_RUL : 'mongodb://127.0.0.1:27017/demos',
-    MD5_SUFFIX : 'Encryption',
-    MAILNAME : 'email-address',
-    MAILPASS : 'email-password',
-    HOSTNAME: 'http://www.demos.so'
-};
