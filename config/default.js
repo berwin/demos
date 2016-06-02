23f015c5362e48379791391f47baf069e11b8846
berwin <berwin1995@qq.com>
2016-06-02 16:57:18 +0800

    添加 helper

--- config/default.js
+++ config/default.js
@@ -12,11 +12,17 @@
 
 var config = {
   port: 1995,
-  MONGO_RUL : 'mongodb://127.0.0.1:27017/demos',
-  MD5_SUFFIX : 'Encryption',
-  MAILNAME : 'email-address',
-  MAILPASS : 'email-password',
-  HOSTNAME: 'http://www.demos.so'
+  mongo_url: 'mongodb://127.0.0.1:27017/demos',
+  md5_suffix: 'Encryption',
+  mailname: 'email-address',
+  mailpass: 'email-password',
+  hostname: 'http://www.demos.so',
+  stat: {
+    url: 'http://stat.berwin.me/api/v1/content',
+    groupID: '55ab38d919a9a46b48bef001',
+    sourceID: '558a2ef3459c3d595db03329',
+    token: '558a2ef3459c3d595db03328'
+  }
 };
 
 module.exports = config;
--- models/base.js
+++ models/base.js
@@ -9,7 +9,7 @@
 
 var config = require('../config/index.js');
 var mongoskin = require('mongoskin');
-var db = mongoskin.db(config.MONGO_RUL, {native_parser: true});
+var db = mongoskin.db(config.mongo_url, {native_parser: true});
 
 exports.getCollection = function (collectionName) {
   return db.collection(collectionName);
