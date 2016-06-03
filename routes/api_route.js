3000070fb452b4bb9360b0c258eed9560b098044
berwin <berwin1995@qq.com>
2016-06-03 20:14:17 +0800

    添加注释

--- routes/api_route.js
+++ routes/api_route.js
@@ -14,14 +14,21 @@ var codeCtrl = require('../controllers/api/code.js');
 var userCtrl = require('../controllers/api/user.js');
 var auth = require('../middleware/auth.js');
 
+// 认证合法性
 router.post('*', auth);
 
+/*
+ * code 相关路由
+ */
 router
   .get('/code', codeCtrl.getCodesByUserId)
   .get('/code/:id/:type', codeCtrl.codeInfo)
   .post('/code', codeCtrl.createCode)
   .delete('/code/:id', codeCtrl.deleteCode);
 
+/*
+ * user 相关路由
+ */
 router
   .post('/user', userCtrl.register)
   .get('/user', userCtrl.getUserInfo)
