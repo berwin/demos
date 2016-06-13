dea5d15250c8c6e4aceb40ff3f787a3df14427d0
berwin <berwin1995@qq.com>
2016-06-13 18:10:31 +0800

    优化代码

--- public/dist/bundle.html.js
+++ public/dist/bundle.html.js
@@ -258,137 +258,122 @@ if(window.console) window.console.log('本产品由 Berwin 独立开发\n开发�
 },{"../base.js":1,"./core.js":2}],4:[function(require,module,exports){
 'use strict';
 
-var tool = require( './utils.js' );
+var tool = require('./utils.js');
 var id = tool.getID();
 
 var menu = {
-
-    getList : function () {
-        return $.get( '/api/code');
-    },
-
-    getUserInfo : function () {
-        return $.get( '/api/user');
-    },
-
-    login : function (mail, password) {
-        return $.post( '/api/login', {id : id, mail : mail, password: password} );
-    },
-
-    register : function (mail, password) {
-        return $.post( '/api/user', {id : id, mail : mail, password : password} );
-    },
-
-    retrieve : function (mail) {
-        return $.post( '/api/retrieve', { id : id, mail : mail } );
-    },
-
-    changepw : function (newPw) {
-        return $.ajax({
-            url: '/api/user',
-            type: 'PUT',
-            data: { id: id, password: newPw }
-        });
-    },
-
-    signout : function () {
-        return $.get( '/api/signout', {id: id} );
-    },
-
-    rmCode : function (data) {
-        return $.ajax({
-            url: '/api/code/' + data.historyID,
-            type: 'DELETE'
-        });
-    }
+  getList: function () {
+    return $.get('/api/code');
+  },
+  getUserInfo: function () {
+    return $.get('/api/user');
+  },
+  login: function (mail, password) {
+    return $.post('/api/login', {id: id, mail: mail, password: password});
+  },
+  register: function (mail, password) {
+    return $.post('/api/user', {id: id, mail: mail, password: password});
+  },
+  retrieve: function (mail) {
+    return $.post('/api/retrieve', {id : id, mail: mail});
+  },
+  changepw: function (newPw) {
+    return $.ajax({
+      url: '/api/user',
+      type: 'PUT',
+      data: {id: id, password: newPw}
+    });
+  },
+  signout: function () {
+    return $.get('/api/signout', {id: id});
+  },
+  rmCode: function (data) {
+    return $.ajax({
+      url: '/api/code/' + data.historyID,
+      type: 'DELETE'
+    });
+  }
 };
 
 var edit = {
-    getCodeInfo : function (type) {
-        return $.get( '/api/code/'+ id +'/' + type );
-    },
-
-    save : function (codeText, type) {
-        return $.post( '/api/code', { id : id, code : codeText, type: type } );
-    }
+  getCodeInfo: function (type) {
+    return $.get('/api/code/'+ id +'/' + type);
+  },
+  save: function (codeText, type) {
+    return $.post('/api/code', {id: id, code: codeText, type: type});
+  }
 };
 
 module.exports = {
-    menu : menu,
-    edit : edit
+  menu: menu,
+  edit: edit
 };
 },{"./utils.js":5}],5:[function(require,module,exports){
 'use strict';
 
 module.exports = {
-    cashe : function () {
-        var data = {};
+  cashe: function () {
+    var data = {};
 
-        return {
-            set : function (name, value) {
-                data[ name ] = value;
-            },
-            get : function (name) {
-                return data[ name ];
-            },
-            rm : function (name) {
-                delete data[ name ];
-            }
-        };
-    },
-    regexp : function () {
-        return {
-            mail : /^([a-z0-9]+[\-|\_|\.]*[\w]*@[a-z0-9\-]+(\.[a-z]{2,3}){1,2})$/i
-        }
-    },
-    cookieToObject : function (str) {
-        var obj = {};
-        var arr = str.split( '; ' );
-        for( var i = 0; i < arr.length; i++ ){
-            var aCookie = arr[i].split( '=' );
-            obj[ aCookie[0] ] = aCookie[1];
-        }
-        return obj;
-    },
-    getCookie : function (name) {
-        var arr = document.cookie.split( '; ' );
-        for( var i = 0; i < arr.length; i++ ){
-            var arr2 = arr[i].split( '=' );
-
-            if( arr2[0] === name ){
-                return arr2[1];
-            }
-        }
-        return '';
-    },
-    setCookie : function (name, value, time) {
-        
-        var str = name + '=' + encodeURIComponent(value);
-
-        if( time ){
-            var oDate = new Date();
-            oDate.setDate(oDate.getDate() + time);
-            str += ';expires=' + oDate;
-        }
-
-        document.cookie = str;
-    },
-    getType : function () {
-        var pathname = window.location.pathname;
-        var type = '';
-
-        if( pathname.indexOf('/js/') === 0 ) type = 'js';
-        if( pathname.indexOf('/html/') === 0 ) type = 'html';
-
-        return type;
-    },
-    getID : function () {
-        var id = '';
-        var pathname = window.location.pathname;
-        if( pathname.indexOf('/js/') === 0 ) id = pathname.substring( 4 );
-        if( pathname.indexOf('/html/') === 0 ) id = pathname.substring( 6 );
-        return id;
+    return {
+      set: function (name, value) {
+        data[name] = value;
+      },
+      get: function (name) {
+        return data[name];
+      },
+      rm: function (name) {
+        delete data[name];
+      }
+    };
+  },
+  regexp: function () {
+    return {
+      mail : /^([a-z0-9]+[\-|\_|\.]*[\w]*@[a-z0-9\-]+(\.[a-z]{2,3}){1,2})$/i
     }
+  },
+  cookieToObject: function (str) {
+    var obj = {};
+    var arr = str.split('; ');
+    for (var i = 0; i < arr.length; i++) {
+      var aCookie = arr[i].split('=');
+      obj[aCookie[0]] = aCookie[1];
+    }
+    return obj;
+  },
+  getCookie: function (name) {
+    var arr = document.cookie.split('; ');
+    for (var i = 0; i < arr.length; i++) {
+      var arr2 = arr[i].split('=');
+      if (arr2[0] === name) {
+        return arr2[1];
+      }
+    }
+    return '';
+  },
+  setCookie: function (name, value, time) {
+    var str = name + '=' + encodeURIComponent(value);
+    if (time) {
+      var oDate = new Date();
+      oDate.setDate(oDate.getDate() + time);
+      str += ';expires=' + oDate;
+    }
+    document.cookie = str;
+  },
+  getType: function () {
+    var pathname = window.location.pathname;
+    var type = '';
+    if( pathname.indexOf('/js/') === 0 ) type = 'js';
+    if( pathname.indexOf('/html/') === 0 ) type = 'html';
+    return type;
+  },
+  getID: function () {
+    var id = '';
+    var pathname = window.location.pathname;
+    if (pathname.indexOf('/js/') === 0) id = pathname.substring(4);
+    if (pathname.indexOf('/html/') === 0) id = pathname.substring(6);
+    return id;
+  }
 };
 },{}],6:[function(require,module,exports){
 'use strict';
--- public/src/helper/requester.js
+++ public/src/helper/requester.js
@@ -1,61 +1,52 @@
 'use strict';
 
-var tool = require( './utils.js' );
+var tool = require('./utils.js');
 var id = tool.getID();
 
 var menu = {
-
-    getList : function () {
-        return $.get( '/api/code');
-    },
-
-    getUserInfo : function () {
-        return $.get( '/api/user');
-    },
-
-    login : function (mail, password) {
-        return $.post( '/api/login', {id : id, mail : mail, password: password} );
-    },
-
-    register : function (mail, password) {
-        return $.post( '/api/user', {id : id, mail : mail, password : password} );
-    },
-
-    retrieve : function (mail) {
-        return $.post( '/api/retrieve', { id : id, mail : mail } );
-    },
-
-    changepw : function (newPw) {
-        return $.ajax({
-            url: '/api/user',
-            type: 'PUT',
-            data: { id: id, password: newPw }
-        });
-    },
-
-    signout : function () {
-        return $.get( '/api/signout', {id: id} );
-    },
-
-    rmCode : function (data) {
-        return $.ajax({
-            url: '/api/code/' + data.historyID,
-            type: 'DELETE'
-        });
-    }
+  getList: function () {
+    return $.get('/api/code');
+  },
+  getUserInfo: function () {
+    return $.get('/api/user');
+  },
+  login: function (mail, password) {
+    return $.post('/api/login', {id: id, mail: mail, password: password});
+  },
+  register: function (mail, password) {
+    return $.post('/api/user', {id: id, mail: mail, password: password});
+  },
+  retrieve: function (mail) {
+    return $.post('/api/retrieve', {id : id, mail: mail});
+  },
+  changepw: function (newPw) {
+    return $.ajax({
+      url: '/api/user',
+      type: 'PUT',
+      data: {id: id, password: newPw}
+    });
+  },
+  signout: function () {
+    return $.get('/api/signout', {id: id});
+  },
+  rmCode: function (data) {
+    return $.ajax({
+      url: '/api/code/' + data.historyID,
+      type: 'DELETE'
+    });
+  }
 };
 
 var edit = {
-    getCodeInfo : function (type) {
-        return $.get( '/api/code/'+ id +'/' + type );
-    },
-
-    save : function (codeText, type) {
-        return $.post( '/api/code', { id : id, code : codeText, type: type } );
-    }
+  getCodeInfo: function (type) {
+    return $.get('/api/code/'+ id +'/' + type);
+  },
+  save: function (codeText, type) {
+    return $.post('/api/code', {id: id, code: codeText, type: type});
+  }
 };
 
 module.exports = {
-    menu : menu,
-    edit : edit
+  menu: menu,
+  edit: edit
 };
